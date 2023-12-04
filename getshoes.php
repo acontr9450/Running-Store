281b22fa77d2c8e26ccf412670bb6d2911ef3490
acontr9450 <acontr9450@users.noreply.github.com>
2023-12-04 12:36:50 -0600

    can get select shoes to be displayed from database using php

--- getshoes.php
+++ getshoes.php
@@ -1,4 +1,50 @@
 <?php
+header('Content-Type:application/json; charset=UTF-8');
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+//Define the address of the server, database, user, and password
+$host = '172.17.0.3';
+$db = 'runningStore';
+$user = 'angel';
+$pass = 'angel767';
+$dsn = "mysql:host=$host;dbname=$db";
+
+//true when the data of only one shoe is required, false otherwise
+$oneShoe = false;
+
+//try to reach/open database
+try {$pdo = new PDO( $dsn, $user, $pass );
+} catch( Exception $e ) {
+die( "<h3>Error in connection: " . $e->getMessage() .
+"</h3>" );
+}
+
+//get data from database
+try {
+    if(!empty( $_GET[ "query" ])){
+        $oneShoe = true;
+    }
+    else{
+        $sql="SELECT DISTINCT Picture, Gender, Brand, Shoe , Price
+            FROM shoes;";
+        $sth = $pdo->query( $sql );
+        $rows = $sth->fetchAll();
+    }
+
+} catch( Exception $e ){
+die( "<h3>Error in query: " . $e ->getMessage() .
+"</h3>" );
+}
+
+$list = array();
+if($oneShoe){
+
+}
+else{
+    foreach( $rows as $row ){
+        array_push( $list, array( $row[0], $row[1], $row[2], $row[3], $row[4] ));
+    }
+}
+$answer = json_encode( $list );
+echo $answer;
 ?>
